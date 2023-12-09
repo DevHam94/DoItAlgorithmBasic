e309e2eada0b29853a2fe2243b0e70515df06d82
admin <gkaeogus7@gmail.com>
2023-12-09 23:37:17 +0900

    dd

---
+++ src/chapter01/Max3Method.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class Max3Method {
+
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if (b > max)
+            max = b;
+        if (c > max)
+            max = c;
+    }
+}
